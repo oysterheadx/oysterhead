b7df7b7de30ac4c11b6d7f93922cab5222ddb2c7
oysterheadx <oysterheadx@gmail.com>
2026-07-31 17:28:34 +0200

    Update works.js

--- js/works.js
+++ js/works.js
@@ -3,114 +3,121 @@ const WORKS = [
   {
     title: "Yin and Yang Triptych",
     file: "yin-and-yang-triptych.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "3×40×40 cm"
   },
   {
     title: "Triptych of Encrypted Messages",
     file: "triptych-of-encrypted-messages.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "3×40×40 cm"
   },
   {
     title: "Flesh Ground Dynamics",
     file: "chaotic-salmon.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "60×40 cm",
   },
   {
     title: "Orange Canvas Splatters",
     file: "squeezing-an-orange.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "40×40 cm",
   },
     {
     title: "All the Tubes I Had Small",
     file: "All The Tubes I Had Small (40x40).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "80 × 80 cm",
   },
   {
     title: "All the Tubes I Had Large",
     file: "All The Tubes I Had Large (50x70).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "50×70 cm",
   },
   {
     title: "Calibrating Circles",
     file: "Colorful Circles (50x50).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "50×50 cm",
   },
   {
     title: "Measuring Circles",
     file: "Measuring Circles (50x70).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "50×70 cm",
   },
   {
     title: "Dual Color Ring",
     file: "Red Blue Splatter (40x40).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "40x40 cm",
   },
   {
     title: "Primary Colors Grid",
     file: "My Basic Color Set (30x30).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "30×30 cm",
+  },
+  {
+    title: "Nine of Red",
+    file: "Nine of Red.jpg",
+    technique: "Acrylic on paper",
+    format: "29.7×42 cm",
+  },
+  {
+    title: "Vertical Sequence of Shapes",
+    file: "Vertical Sequence of Shapes.jpg",
+    technique: "Acrylic on paper",
+    format: "15×42 cm",
   },
   {
     title: "Contrast in Motion Small",
     file: "from-fire-to-water-30.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "30×30 cm",
   },
   {
     title: "Contrast in Motion Large",
     file: "from-fire-to-water-50.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "50×70 cm",
   },
   {
     title: "Abstract Spectrum Skyline",
     file: "palette-of-drops.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "60×40 cm",
   },  
   {
     title: "Distorted Red Cross",
     file: "broken-cross.jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "40×40 cm",
+  },
+  {
+    title: "Abstract Calligraphy Signs",
+    file: "Abstract Calligraphy Signs.jpg",
+    technique: "Acrylic on canvas",
+    format: "25×25 cm",
+  },
+  {
+    title: "Heavy Impasto Marks",
+    file: "Heavy Impasto Marks.jpg",
+    technique: "Acrylic on paper",
+    format: "29.7×21 cm",
   },
   {
     title: "Golden Texture in Dark",
     file: "Golden Autumn (40x40).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "40×40 cm",
   },
   {
     title: "Blue Canvas with X",
     file: "White Crosses (40x40).jpg",
-    technique: "Acrylic on paper",
+    technique: "Acrylic on canvas",
     format: "40×40 cm",
   }
-
- /*
-
-
-  {
-    title: "Zapomniane miasto nocy",
-    file: "work-11.svg",
-    technique: "olej na płótnie",
-    format: "100 × 100 cm",
-  },
-  {
-    title: "Ostatni promień lata",
-    file: "work-12.svg",
-    technique: "akryl na płótnie",
-    format: "70 × 90 cm",
-  }
-  */
 ];
